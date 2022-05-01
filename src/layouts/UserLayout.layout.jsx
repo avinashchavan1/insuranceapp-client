6e51b7cfe631f20e7581169ca465aaeb5145cf08
Avinash Chavan <avinashchavan1443@gmail.com>
2022-05-01 11:58:57 +0530

    User layout added

---
+++ src/layouts/UserLayout.layout.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Divider, Layout, Menu } from "antd";
+import { useNavigate } from "react-router-dom";
+const { Header, Footer } = Layout;
+
+export default (props) => {
+  const navigate = useNavigate();
+  const clickHandler = (event) => {
+    console.log("clicked", event.key);
+    navigate(event.key);
+  };
+  const menuItems = [
+    { label: "Home", key: "/", onClick: clickHandler, value: "/" },
+    {
+      label: "Dashboard",
+      key: "/dashboard",
+      onClick: clickHandler,
+      value: "/dashboard",
+    },
+  ];
+  return (
+    <Layout>
+      <Header
+        style={{
+          zIndex: 1,
+          width: "100%",
+        }}
+      >
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["/"]}
+          items={menuItems}
+        />
+      </Header>
+      <Divider />
+
+      {props.children}
+      <Divider />
+
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Insurance Analytics ©2022 Created by Avinash Chavan
+      </Footer>
+    </Layout>
+  );
+};
--- src/pages/LandingPage/PageNotFound.page.jsx
+++ src/pages/LandingPage/PageNotFound.page.jsx
@@ -1,15 +1,18 @@
 import { Result, Button } from "antd";
+import UserLayoutLayout from "../../layouts/UserLayout.layout";
 
 const PageNotFound = () => (
-  <Result
-    status="404"
-    title="404"
-    subTitle="Sorry, the page you visited does not exist."
-    extra={
-      <Button type="primary" href="/">
-        Back Home
-      </Button>
-    }
-  />
+  <UserLayoutLayout>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary" href="/">
+          Back Home
+        </Button>
+      }
+    />
+  </UserLayoutLayout>
 );
 export default PageNotFound;
